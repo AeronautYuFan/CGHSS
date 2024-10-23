4404b2caee838737723f972217e22912fc1062e0
AeronautYuFan <aeronautyufan@gmail.com>
2024-10-23 15:29:51 -0400

    remove export copy

--- uscg temp export/data-cleaning/file_utils.py
+++
@@ -1,8 +0,0 @@
-# file_utils.py
-
-# Nothing fancy rn, this method is meant to hold the file name
-#
-# I made this just so if I ever have 24601 different scripts,
-# there's an easy way to change the file name for all of them
-def get_airtable():
-    return "fullDataset.csv"
